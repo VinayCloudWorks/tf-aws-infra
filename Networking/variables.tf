e0c4cee47d2c613fd39be2b48f2cce534d3a7d29
Vinay Sathe <sathe.v@northeastern.edu>
2025-02-19 17:29:44 -0500

    Pass check

--- Networking/variables.tf
+++ Networking/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
-       description = "AWS region to deploy resources in"
-                      type        = string
+  description = "AWS region to deploy resources in"
+  type        = string
   default     = "us-east-1"
 }
 
