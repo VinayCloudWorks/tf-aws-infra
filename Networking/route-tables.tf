ade9d39169fd06455427a8801e61b976149d11e9
Vinay Sathe <sathe.v@northeastern.edu>
2025-02-19 15:11:44 -0500

    Multiple VPCs logic added (#3)
    * Folder and initial working code added
    
    * GitHub Actions workflow added
    
    * Multiple VPCs logic added

--- Networking/route-tables.tf
+++ Networking/route-tables.tf
@@ -1,34 +1,40 @@
-# Public Route Table with a route to the Internet Gateway
+# Public Route Tables with a default route to the IGW
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  for_each = aws_vpc.main
+
+  vpc_id = each.value.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.this[each.key].id
   }
 
   tags = {
-    Name = "${var.vpc_name}-public-rt"
+    Name = "${each.value.tags["Name"]}-public-rt"
   }
 }
 
 resource "aws_route_table_association" "public" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public[each.value.tags["vpc_key"]].id
 }
 
-# Private Route Table (without an Internet Gateway route)
+# Private Route Tables (without a default route to an IGW)
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+  for_each = aws_vpc.main
+
+  vpc_id = each.value.id
 
   tags = {
-    Name = "${var.vpc_name}-private-rt"
+    Name = "${each.value.tags["Name"]}-private-rt"
   }
 }
 
 resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private[each.value.tags["vpc_key"]].id
 }
--- Networking/vpc.tf
+++ Networking/vpc.tf
@@ -1,9 +1,11 @@
 resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
+  for_each = var.vpcs
+
+  cidr_block           = each.value.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = var.vpc_name
+    Name = each.value.vpc_name
   }
 }
